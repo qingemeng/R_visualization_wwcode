1191879bae03dd50951ff8ef828eafb01898f813
Gemeng <qingemeng@gmail.com>
2017-11-20 21:14:23 +0800

    Add plots based on genre

--- imdb.R
+++ imdb.R
@@ -25,7 +25,7 @@ runtime_rating_plot +
 # remove very long movies
 normal_length_movies <- movies %>% filter(runtime <= 240 & runtime > 0) 
 ggplot(normal_length_movies, aes(runtime, rating)) +
-  geom_point()
+  geom_point(colour = 'blue')
 
 # rating, runtime over time
 ggplot(normal_length_movies, aes(runtime, rating)) +
@@ -46,8 +46,44 @@ normal_length_movies <- normal_length_movies %>%
 
 plot <- ggplot(normal_length_movies, aes(x = release_date, y = runtime)) +
   geom_point(aes(colour = rating)) +
-  scale_color_gradient(low = 'yellow', high = 'red')
+  scale_color_gradient(low = 'yellow', high = 'red') +
+  xlab('release date') +
+  ylab('length') + 
+  ggtitle("runtime vs rating over years")
 
 ggplotly(plot)
 
-# plotly
+# genre
+head(movies$genres)
+
+devtools::install_github("sailthru/tidyjson")
+library('tidyjson')
+# convert and gether json
+movies2 <- movies %>% 
+  as.tbl_json(json.column = "genres") %>% 
+  gather_array() %>% 
+  spread_values(genre=jstring('name'))
+
+g_genre <- ggplot(movies2, aes(genre, rating))
+g_genre + geom_boxplot(aes(fill = genre))
+
+# get frequecy by gener
+genre_frequencies <- movies2 %>%
+  group_by(genre) %>% 
+  summarize(count=n())
+
+ggplot(genre_frequencies, aes(genre,count)) +
+  geom_bar(stat = 'identity')
+
+#get rating of action movies over year
+action_movies <- subset(movies2, genre = 'Action') 
+g_action_movies_plot <- ggplot(action_movies, aes(release_date, rating))
+g_action_movies_plot + 
+  geom_point() +
+  geom_smooth()
+
+movies3 <- filter(movies2, genre == 'Action' | genre == 'Drama' | genre == 'Animation')
+p_movies3 <- ggplot(movies3, aes(release_date, rating))
+p_movies3 + 
+  geom_point(aes(color = genre)) +
+  facet_grid(genre~.)
