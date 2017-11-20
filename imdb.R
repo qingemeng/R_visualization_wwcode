05074308443ffcf5e843a5058a9d2d075b7041df
Gemeng <qingemeng@gmail.com>
2017-11-20 21:51:40 +0800

    Refactor var names

--- imdb.R
+++ imdb.R
@@ -3,7 +3,6 @@ library(dplyr)
 library(plotly)
 
 movies <- read_csv('./data/data_raw/tmdb-5000-movie-dataset/tmdb_5000_movies.csv')
-#credits <- read_csv('./data/data_raw/tmdb-5000-movie-dataset/tmdb_5000_credits.csv')
 colnames(movies)[colnames(movies) == 'vote_average'] <- 'rating'
 colnames(movies)
 
@@ -12,39 +11,35 @@ movies <- movies %>%
   filter(!is.na(release_date))
 
 colnames(movies)
-#colnames(credits)
 
-head(movies, 1)
+#head(movies, 1)
+#dim(movies)
 
-dim(movies)
 # rating, length
 runtime_rating_plot <- ggplot(data = movies, aes(x = runtime, y = rating))
 runtime_rating_plot +
   geom_point()
 
 # remove very long movies
-normal_length_movies <- movies %>% filter(runtime <= 240 & runtime > 0) 
-ggplot(normal_length_movies, aes(runtime, rating)) +
+tidy_movies <- movies %>% filter(runtime <= 240 & runtime > 0) 
+ggplot(tidy_movies, aes(runtime, rating)) +
   geom_point(colour = 'blue')
 
-# rating, runtime over time
-ggplot(normal_length_movies, aes(runtime, rating)) +
-  geom_point(aes(colour = release_date)) +
-  scale_color_gradient(low = 'yellow', high = 'red')
+#zero rating
+tidy_movies %>% ggplot(aes(rating, vote_count)) +
+  geom_bar(stat = "identity")
 
-ggplot(normal_length_movies, aes(x = release_date, y = rating)) +
+
+tidy_movies <- tidy_movies %>% 
+  filter(rating > 2.5 & rating < 9)
+
+# rating, runtime over time
+ggplot(tidy_movies, aes(x = release_date, y = rating)) +
   geom_point(aes(colour = runtime)) +
   scale_color_gradient(low = 'yellow', high = 'red')
 
-#zero rating
-normal_length_movies %>% ggplot(aes(rating, vote_count)) +
-  geom_bar(stat = "identity")
-
-normal_length_movies <- normal_length_movies %>% 
-  filter(rating > 2.5) %>% 
-  filter(rating < 9)
-
-plot <- ggplot(normal_length_movies, aes(x = release_date, y = runtime)) +
+# To do 
+plot <- ggplot(tidy_movies, aes(x = release_date, y = runtime)) +
   geom_point(aes(colour = rating)) +
   scale_color_gradient(low = 'yellow', high = 'red') +
   xlab('release date') +
@@ -56,19 +51,19 @@ ggplotly(plot)
 # genre
 head(movies$genres)
 
-devtools::install_github("sailthru/tidyjson")
+# devtools::install_github("sailthru/tidyjson")
 library('tidyjson')
 # convert and gether json
-movies2 <- movies %>% 
+movies_by_genre <- movies %>% 
   as.tbl_json(json.column = "genres") %>% 
   gather_array() %>% 
   spread_values(genre=jstring('name'))
 
-g_genre <- ggplot(movies2, aes(genre, rating))
+g_genre <- ggplot(movies_by_genre, aes(genre, rating))
 g_genre + geom_boxplot(aes(fill = genre))
 
 # get frequecy by gener
-genre_frequencies <- movies2 %>%
+genre_frequencies <- movies_by_genre %>%
   group_by(genre) %>% 
   summarize(count=n())
 
@@ -76,14 +71,13 @@ ggplot(genre_frequencies, aes(genre,count)) +
   geom_bar(stat = 'identity')
 
 #get rating of action movies over year
-action_movies <- subset(movies2, genre = 'Action') 
+action_movies <- subset(movies_by_genre, genre = 'Action') 
 g_action_movies_plot <- ggplot(action_movies, aes(release_date, rating))
 g_action_movies_plot + 
   geom_point() +
-  geom_smooth()
-
-movies3 <- filter(movies2, genre == 'Action' | genre == 'Drama' | genre == 'Animation')
-p_movies3 <- ggplot(movies3, aes(release_date, rating))
-p_movies3 + 
+  geom_smooth(method = 'auto')
+movies_by_genre_sub <- filter(movies_by_genre, genre == 'Action' | genre == 'Science Fiction' | genre == 'Animation')
+ggplot(movies_by_genre_sub, aes(release_date, budget)) +
   geom_point(aes(color = genre)) +
+  geom_smooth() +
   facet_grid(genre~.)
